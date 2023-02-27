5703c259c9b3ce11a8fd4bfa2847ee9d8c575268
Gustavo Bryan <devgustavob@gmail.com>
2023-02-27 13:26:41 -0300

    :bricks:ci Adição de itens na infraestrutura  html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,8 @@
 async function getProdutos() {
-    let response = await fetch(`http://localhost:3000/produtos`);
+    let response = await fetch(`http://localhost:3000/produtos/${Acessorios}`);
     let userData = await response.json();
     const produtoId = userData;
-    console.log(userData[1]);
+    console.log(userData);
     
 }
    getProdutos();
